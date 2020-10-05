079d1a3af888e90c8b662802e240cb1522a3d611
ARDRA SALEMY <inf_asalemy@tractorsupply.com>
2020-10-05 16:41:46 -0400

    first

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createPost, editPost, setFilter } from './actions'
+
+console.log(editPost('100', 'Lots of text yada yada'));
+console.log(createPost('dan', 'New post'));
+console.log(setFilter('yada'));
